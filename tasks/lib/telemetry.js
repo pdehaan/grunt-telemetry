db037dcede6dd0a0d93991f8c5fbcd98c2f08bef
Parashuram <code@nparashuram.com>
2013-10-20 18:48:42 -0700

    Refactoring to move the server

--- tasks/lib/telemetry.js
+++ tasks/lib/telemetry.js
@@ -1,25 +1,29 @@
-var fs = require('fs');
-
 module.exports = (function() {
-	var express = require('express');
-	var app = express();
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	var browserConfig = null;
+	var browserConfig = null, browsers = {
+		'chrome' : {
+			browser : 'chrome',
+			options : [ '--enable-gpu-benchmarking', '--enable-threaded-compositing', '--cancel-first-run', '--bwsi', '--no-first-run' ]
+		},
+		'ie' : {
+			browser : 'ie',
+		// options : ['-extoff', '-private']
+		},
+		'firefox' : {
+			browser : 'firefox'
+		}
+	};
 
-	// Takes a page component and inserts all the telemetry scripts into the
-	// page
-
+	/**
+	 * Takes a page component and inserts all the telemetry scripts into the page
+	 */
 	function telemetryPage(page) {
-		var pageScripts = [ '\n<script type = "text/javascript">\n' ];
+		var fs = require('fs'), pageScripts = [ '\n<script type = "text/javascript">\n' ];
 		[ 'smoothness_measurement.js', 'scroll.js', 'benchmarks.js' ].forEach(function(file) {
 			pageScripts.push(fs.readFileSync(__dirname + '/page_scripts/' + file, 'utf-8'));
 		});
 		pageScripts.push('</script>');
 
 		var html = fs.readFileSync(page, 'utf-8');
-
-		html.replace()
 
 		if (html.match(/<head>/)) {
 			html = html.replace(/<head>/, '<head>' + pageScripts.join(''));
@@ -34,41 +38,30 @@ module.exports = (function() {
 		return html;
 	}
 
-	// Serves the pages that need to be tested
+	/**
+	 * Serves the pages that need to be tested
+	 */
+	function runServer(page, cb) {
+		var express = require('express');
+		var app = express();
+		app.use(express.bodyParser());
+		app.use(express.methodOverride());
 
-	function servePage(page) {
-		app.use('/tests/', express.static(__dirname + '/../tests/'));
 		app.get('/', function(req, res) {
 			res.send(telemetryPage(page));
 		});
-	}
 
-	// URL where the data is posted
-
-	function collectData(page, cb) {
-		console.log('Listening for data for', page);
 		app.post('/data', function(req, res) {
-			console.log('Got telemetry data from the page');
 			var data = req.body;
 			data['url'] = page;
-			cb(data);
 			res.send('');
+			if (typeof cb === 'function') {
+				server.close();
+				cb(data);
+			}
 		});
+		var server = app.listen(3000);
 	}
-
-	var browsers = {
-		'chrome' : {
-			browser : 'chrome',
-			options : [ '--enable-gpu-benchmarking', '--enable-threaded-compositing', '--cancel-first-run', '--bwsi', '--no-first-run' ]
-		},
-		'ie' : {
-			browser : 'ie',
-		// options : ['-extoff', '-private']
-		},
-		'firefox' : {
-			browser : 'firefox'
-		}
-	};
 
 	function openPageInBrowser(browserName, page, cb) {
 		if (typeof cb !== 'function') {
@@ -77,10 +70,10 @@ module.exports = (function() {
 		var launcher = require('browser-launcher');
 		launcher(browserConfig || {}, function(err, launch) {
 			if (err) {
-				cb(false);
+				cb(err);
 			} else {
 				launch('http://localhost:3000/?' + Math.random(), browsers[browserName], function(err, ps) {
-					cb(typeof ps === 'undefined' ? false : ps);
+					cb(err, ps);
 				});
 			}
 		});
@@ -88,45 +81,45 @@ module.exports = (function() {
 
 	return {
 		setup : function(opts) {
-			browserConfig = opts
-		},
-
-		serve : function(page) {
-			console.log('Serving page at 3000');
-			servePage(page);
-			app.listen(3000);
-		},
-
-		run : function(page, browserName, cb) {
-			servePage(page);
-			var browser = null;
-			collectData(page, function(data) {
-				console.log('Data collected from the browser');
-				close(data);
-			});
-
-			function close(data) {
-				server.close();
-				if (browser) {
-					console.log('Closing Browser');
-					browser.kill();
-				}
-				cb(data);
-			}
-
-			var server = app.listen(3000);
-			openPageInBrowser(browserName, page, function(ps) {
-				if (ps === false) {
-					close();
-				} else {
-					browser = ps;
-				}
-			});
+			browserConfig = opts;
 		},
 
 		availableBrowsers : function() {
 			return browsers;
+		},
+
+		serve : function(page) {
+			runServer(page);
+		},
+
+		run : function(page, browserName, cb) {
+			var browser = null, closed = false;
+			var close = function(data) {
+				if (closed) {
+					return;
+				}
+				browser && browser.kill();
+				closed = true;
+				cb(data);
+			}
+
+			runServer(page, function(data) {
+				close(data);
+			});
+
+			openPageInBrowser(browserName, page, function(err, ps) {
+				if (ps) {
+					ps.on('exit', function() {
+						close();
+					});
+					browser = ps;
+				} else {
+					console.log(err);
+					close();
+				}
+			});
 		}
+
 	}
 
 }());
--- test/test.js
+++ test/test.js
@@ -1,12 +1,17 @@
 var assert = require("assert"), expect = require('expect.js'), telemetry = require('../tasks/lib/telemetry');
+telemetry.setup({
+	"config" : __dirname + '/config.json'
+});
 
 describe('When testing browsers', function() {
 	this.timeout(0);
-	it('should launch firefox', function(done) {
-		telemetry.run(__dirname + '/1.html', 'chrome', function(data) {
-			expect(data).to.have.property('load_time_ms');
-			expect(data).to.have.property('dom_content_loaded_time_ms');
-			done();
+	['firefox' ].forEach(function(browser) {
+		it('should launch ' + browser, function(done) {
+			telemetry.run(__dirname + '/1.html', browser, function(data) {
+				expect(data).to.have.property('load_time_ms');
+				expect(data).to.have.property('dom_content_loaded_time_ms');
+				done();
+			});
 		});
-	})
+	});
 })
